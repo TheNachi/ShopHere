9001d8a031e0da58c45c007652e935dcfae173b2
Munachimso Ugorji <ugorjimunachimso@gmail.com>
2024-12-07 17:53:51 +0100

    Initial Commit

---
+++ ShopHere/ShopHereApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopHereApp.swift
+//  ShopHere
+//
+//  Created by Munachimso Ugorji on 07/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShopHereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
